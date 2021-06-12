279e12320d9b02f071ef9bf17c90d7671230e572
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-12 14:44:07 -0400

    Update attributes for CatPic model.

--- db/migrate/20210611211406_create_cat_pics.rb
+++ db/migrate/20210611211406_create_cat_pics.rb
@@ -1,8 +1,12 @@
 class CreateCatPics < ActiveRecord::Migration[6.1]
   def change
     create_table :cat_pics do |t|
-      t.string :title
-      t.string :link
+      t.string :description
+      t.string :thumbnail
+      t.string :source
+      t.integer :viewcount
+      t.boolean :animated
+      t.datetime :posted_at
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_211406) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "cat_pics", force: :cascade do |t|
-    t.string "title"
-    t.string "link"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
