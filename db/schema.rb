1f33b4fb57bbb7a7c6eeab485813770150925b14
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-12 15:00:44 -0400

    Create cat_pics table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_06_11_211406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cat_pics", force: :cascade do |t|
+    t.string "description"
+    t.string "thumbnail"
+    t.string "source"
+    t.integer "viewcount"
+    t.boolean "animated"
+    t.datetime "posted_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
