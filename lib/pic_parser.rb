90849401ae0a15995834a426f8c744924d6adf96
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-12 22:40:10 -0400

    Debug and handle exceptions.

--- lib/pic_parser.rb
+++ lib/pic_parser.rb
@@ -1,26 +1,15 @@
 module PicParser
 
   def scrape_by_period(url, period)
-    options = {
-      all_time: 'all',
-      year: 'year',
-      month: 'month',
-      week: 'week',
-      day: 'day'
-    }
-    binding.pry
-    scrape(url + '/' + options[period]) if options[period]
-    puts "Invalid period supplied." if !options[period]
-
+    scrape(url + "/#{period}")
   end
 
   def scrape(base_url)
-
     page_num = 0
     while page_num < 5
       html = HTTParty.get(base_url + "/page/#{page_num}")
       doc = Nokogiri::HTML(html.body)
-      posts = doc.css('div.post')
+      posts = doc.css('div.post:not(.empty)')
       
       posts.each do |post|
         parsed = parse_data(post)
@@ -34,7 +23,7 @@ module PicParser
 
   def parse_data(post)
     data = Hash.new
-    data[:imgur_id] = post.attributes['id'].value
+    data[:imgur_id] = post.attributes['id'].value rescue return
     data[:description] = post.css('div.hover p')[0].content
 
     info = post.css('div.post-info')[0].content
@@ -45,11 +34,7 @@ module PicParser
   end
 
   def parse_viewcount(string)
-    begin
-      string.strip[/(\d*\,?\d*\,?\d+)|ø/].delete(',').to_i
-    rescue NoMethodError
-      nil
-    end
+    string.strip[/(\d*\,?\d*\,?\d+)|ø/].delete(',').to_i
   end
 
 end
