5883bcc682910609a20f0f7872f6bc7085822291
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-07-09 15:07:25 -0400

    Add ActiveRecord relationships to models.

--- app/models/cat_pic.rb
+++ app/models/cat_pic.rb
@@ -1,3 +1,5 @@
 class CatPic < ApplicationRecord
   validates :imgur_id, uniqueness: true
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  has_many :likes, as: :likeable
+  has_one :cat_pic
 end
