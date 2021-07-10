ebd83bb5d4169ca0a65d25a1a9218ed9f46f0ad4
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-07-10 13:32:20 -0400

    Debug CatPic ActiveRecord relationships. Add get_random class method and url instance method.

--- app/models/cat_pic.rb
+++ app/models/cat_pic.rb
@@ -1,5 +1,11 @@
 class CatPic < ApplicationRecord
   validates :imgur_id, uniqueness: true
-  belongs_to :user
-  belongs_to :post
+
+  def self.get_random
+    CatPic.order(Arel.sql('RANDOM()')).limit(1)[0]
+  end
+
+  def url
+    self.animated ? "https://www.imgur.com/#{self.imgur_id}.mp4" : "https://www.imgur.com/#{self.imgur_id}.jpeg"
+  end
 end
