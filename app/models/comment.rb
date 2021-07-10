74e1bae3f0fb7f265f0f0942d53a5d09cca0328f
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-07-10 13:32:43 -0400

    Add get_random class methods.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
   has_many :likes, as: :likeable
+
+  def self.get_random
+    Comment.order(Arel.sql('RANDOM()')).limit(1)[0]
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes, as: :likeable
   has_one :cat_pic
+
+  def self.get_random
+    Post.order(Arel.sql('RANDOM()')).limit(1)[0]
+  end
+
 end
