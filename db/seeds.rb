fa0ca5f70073f83e9beb547397c12fb28f2624d5
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-07-10 13:33:58 -0400

    Create seed file using Faker gem.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating 50 users."
+50.times do
+  username = Faker::Internet.unique.username(separators: %w(. _ -))
+  email = Faker::Internet.unique.email(name: username)
+
+  User.create(username: username, email: email, password: "password")
+end
+
+puts "Creating a post for each Cat Pic."
+CatPic.all.each do |pic|
+  user = User.get_random
+
+  Post.create(cat_pic: pic, caption: pic.description, user: user)
+end
+
+puts "Creating Comments for each Post."
+Post.all.each do |post|
+  rand(4).times do
+    user = User.get_random
+    text = Faker::Lorem.paragraph(sentence_count: 2, random_sentences_to_add: 3)
+
+    Comment.create(user: user, text: text, post: post)
+  end
+end
+
+puts "Make each User like 5 Posts and 5 Comments."
+User.all.each do |user|
+  5.times do
+    Like.create(user: user, likeable: Post.get_random)
+    Like.create(user: user, likeable: Comment.get_random)
+  end
+end
