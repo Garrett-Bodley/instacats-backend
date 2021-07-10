d9b6a101be3446ddd120fdf8e8072dd0065db22c
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-07-10 13:32:58 -0400

    Debug get_random method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :posts
   has_many :comments
   has_many :likes
 
   def self.get_random
-    User.order(Arel.sql('RANDOM()')).limit(1)
+    User.order(Arel.sql('RANDOM()')).limit(1)[0]
   end
 end
