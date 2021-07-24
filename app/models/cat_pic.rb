d8156dc88db24bd04176824f7c4de91c78f7e4cf
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-07-24 16:00:52 -0400

    Write method to return page_url.

--- app/models/cat_pic.rb
+++ app/models/cat_pic.rb
@@ -5,7 +5,11 @@ class CatPic < ApplicationRecord
     CatPic.order(Arel.sql('RANDOM()')).limit(1)[0]
   end
 
-  def url
+  def src_url
     self.animated ? "https://www.imgur.com/#{self.imgur_id}.mp4" : "https://www.imgur.com/#{self.imgur_id}.jpeg"
+  end
+
+  def page_url
+    "https://www.imgur.com/r/cats/#{self.imgur_id}"
   end
 end
