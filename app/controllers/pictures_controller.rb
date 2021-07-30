ced8ac3518756ed979037a3a32a1f9e14a6d4330
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-07-30 10:57:41 -0400

    Build out basic index action.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,10 @@
 class PicturesController < ApplicationController
 
   def index
-    binding.pry
+    page = !!params[:page] ? params[:page] : 1
+    pics = CatPic.order_by_new.page(page).per(5)
+    render json: pics
   end
+
+
 end
