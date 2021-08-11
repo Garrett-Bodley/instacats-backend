5c976be364e25f943a2491ec4744d67cd2849f05
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-08-11 17:48:59 -0400

    Update seed file to reflect removal of Post model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,26 @@ puts "Creating 50 users."
   User.create(username: username, email: email, password: "password")
 end
 
-puts "Creating a post for each Cat Pic."
+puts "Assigning a User to each CatPic"
 CatPic.all.each do |pic|
-  user = User.get_random
-
-  Post.create(cat_pic: pic, caption: pic.description, user: user)
+  pic.user = User.get_random
+  pic.save
 end
 
-puts "Creating Comments for each Post."
-Post.all.each do |post|
+puts "Creating Comments for each Pic."
+CatPic.all.each do |pic|
   rand(4).times do
     user = User.get_random
     text = Faker::Lorem.paragraph(sentence_count: 2, random_sentences_to_add: 3)
 
-    Comment.create(user: user, text: text, post: post)
+    Comment.create(user: user, text: text, cat_pic: pic)
   end
 end
 
-puts "Make each User like 5 Posts and 5 Comments."
+puts "Make each User like 5 Pics and 5 Comments."
 User.all.each do |user|
   5.times do
-    Like.create(user: user, likeable: Post.get_random)
+    Like.create(user: user, likeable: CatPic.get_random)
     Like.create(user: user, likeable: Comment.get_random)
   end
 end
