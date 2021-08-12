a9a9847affc1d734c42120f7247bb086e5fb903a
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-08-12 15:54:00 -0400

    Write custom method for user attribute.

--- app/serializers/cat_pic_serializer.rb
+++ app/serializers/cat_pic_serializer.rb
@@ -1,3 +1,7 @@
 class CatPicSerializer < ActiveModel::Serializer
-  attributes :id, :imgur_id, :src_url, :description, :viewcount, :animated, :posted_at
+  attributes :id, :imgur_id, :src_url, :description, :viewcount, :animated, :posted_at, :user
+
+  def user
+    object.user.username if !!object.user
+  end
 end
