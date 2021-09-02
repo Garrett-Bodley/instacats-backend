e96974b94fd61c03fade40e0e2bfd693357bd700
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-09-01 21:19:41 -0400

    Refactor custom likes method. Simplified to only return an array (no meta attributes).

--- app/serializers/cat_pic_serializer.rb
+++ app/serializers/cat_pic_serializer.rb
@@ -1,28 +1,10 @@
 class CatPicSerializer < ActiveModel::Serializer
   attributes :id, :imgur_id, :src_url, :description, :viewcount, :animated, :posted_at, :user, :likes
 
-
   def likes
-    if object.likes.count > 0
-      response = {
-        count: object.likes.count,
-        likes: serialize_likes(object.likes)
-      }
-      return response
-    else
-      return nil
-    end
-  end
-
-  def user
-    object.user.username if !!object.user
-  end
-
-  private
-
-  def serialize_likes(likes)
     serialized = []
-    likes.each do |like|
+    return serialized if object.likes.count == 0
+    object.likes.each do |like|
       data = {
         id: like.id,
         user: {
@@ -34,6 +16,10 @@ class CatPicSerializer < ActiveModel::Serializer
       serialized << data
     end
     return serialized
+  end
+
+  def user
+    object.user.username if !!object.user
   end
 
 end
