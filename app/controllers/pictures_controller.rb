5048fd47a70a4f4c2dcc49bcc26723b7b3ac0754
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-09-03 23:24:16 -0400

    Order CatPics by likes (for development purposes).

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,7 +2,7 @@ class PicturesController < ApplicationController
 
   def index
     page = !!params[:page] ? params[:page] : 1
-    pics = CatPic.order_by_new.page(page).per(5)
+    pics = CatPic.order_by_likes.page(page).per(5)
     render json: pics
   end
 
