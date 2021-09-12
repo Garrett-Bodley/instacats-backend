0842560510c2aacb0516c280b45c890a04d8ac0b
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-09-12 16:19:08 -0400

    Add an order_by_likes scope method.

--- app/models/cat_pic.rb
+++ app/models/cat_pic.rb
@@ -6,7 +6,8 @@ class CatPic < ApplicationRecord
   has_many :likes, as: :likeable, dependent: :destroy
   
   scope :order_by_new, -> { order(posted_at: :desc) }
-  scope :order_by_likes, -> { joins(:likes).group('cat_pics.id').order('COUNT(likes.likeable_id) DESC')}
+  scope :order_by_likes, -> { joins(:likes).group('cat_pics.id').order('COUNT(likes.likeable_id) DESC') }
+  scope :order_by_most_commented, -> { joins(:comments).group('cat_pics.id').order('COUNT(comments.cat_pic_id) DESC') }
 
   def self.get_random
     CatPic.order(Arel.sql('RANDOM()')).limit(1)[0]
